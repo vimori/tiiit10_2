a6f25d314caef235e9613b9600c8458604c05693
Ilay <ilay.vabischevich@gmail.com>
2025-04-29 15:49:30 +0300

    исправление ошибок

--- bubble.cpp
+++ bubble.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 
 int main() {
-    int arr[] = {64, 34, 34, 25, 61, 12, 22, 11, 90, 42, 99};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    
-    cout << "Original: ";
+    int arr[] = { 64, 34, 34, 25, 61, 12, 22, 11, 90, 42, 99 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i << " ";
+        std::cout << arr[i] << " ";
     }
-    cout << endl;
-    
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
+    std::cout << std::endl;
+
+    // Сортировка пузырьком
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    
-    cout < "Output: ";
+
+    std::cout << "Output: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        std::cout << arr[i] << " ";
     }
-    cout << endl;
-    
+    std::cout << std::endl;
+
     return 0;
 }
